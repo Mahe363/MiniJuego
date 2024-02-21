c7ba18cd3beb6be691a12c9f76892d996fb2138d
Ángel Pérez Mengual <angel.perez.mengual@ces-vegamedia.es>
2024-02-21 23:34:04 +0100

    Broncas

--- VegaJuego/Level1.cs
+++ VegaJuego/Level1.cs
@@ -6,20 +6,53 @@ namespace VegaJuego
     public partial class Level1 : Form
     {
         Link Player;
+        Broncas Enemigo1;
         public Level1()
         {
             InitializeComponent();
             IniciarPersonajes();
+            IniciarOpcionesDelMapa();
         }
         private void IniciarPersonajes()
         {
             Player = new Link(this, 0, 0, 30, 0, 0);
+            Enemigo1 = new Broncas(this, 0, 300);
+        }
+
+        public void IniciarOpcionesDelMapa()
+        {
+            labelVida.Text = "Salud->" + Player.Salud;
+            labelEscudo.Text = "Escudo->" + Player.Escudo;
+            labelArma.Text = "Arma->" + Player.Arma;
         }
 
         private void Level1_KeyPress(object sender, KeyPressEventArgs e)
         {
             Player.mover(e);
-            Debug.WriteLine("[X" + Player.Caja.Location.X + ", Y"+Player.Caja.Location.Y+"]");
+            Debug.WriteLine("[X" + Player.Caja.Location.X + ", Y" + Player.Caja.Location.Y + "]");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelVida.Text = "Salud->" + Player.Salud;
+            labelEscudo.Text = "Escudo->" + Player.Escudo;
+            labelArma.Text = "Arma->" + Player.Arma;
+
+            if(Player.Caja.Bounds.IntersectsWith(Enemigo1.Caja.Bounds)) 
+            {
+                Player.Salud = Player.Salud - 1;
+            }
+            Enemigo1.mover();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
