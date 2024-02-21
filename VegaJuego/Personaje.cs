52539ceb818d74e92e03283fa27e22f353f64277
Ángel Pérez Mengual <angel.perez.mengual@ces-vegamedia.es>
2024-02-21 22:05:27 +0100

    Bug fix

--- VegaJuego/Personaje.cs
+++ VegaJuego/Personaje.cs
@@ -55,6 +55,7 @@ namespace VegaJuego
             lienzo = vegaZelda;
             direccion = "Derecha";
             caja.Location = new System.Drawing.Point(coordX, coordY);
+            caja.BackColor = System.Drawing.Color.Transparent;
             caja.Size = new System.Drawing.Size(110, 110);
             caja.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             caja.BackgroundImage = global::VegaJuego.Properties.Resources.derecha;
